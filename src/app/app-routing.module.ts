a7a03c36a9e8f48a7a7fd81795a227d5edb302a1
IgorDv5 <igoraluno2005@gmail.com>
2024-03-11 21:06:51 -0300

    Rota Para Login Criada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
 import { UsuarioListComponent } from './components/usuario/usuario-list/usuario-list.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  
   {path: '', component:HeaderComponent, children:[
     {path: 'home', component: HomeComponent},
     {path: 'usuarios', component: UsuarioListComponent},
