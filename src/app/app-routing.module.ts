4b2fba9dad4eeac79821e3e048fd61602f3a506d
IgorDv5 <igoraluno2005@gmail.com>
2024-03-17 14:36:18 -0300

    Rota Para Usuario-edit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { AuthGuard } from './auth/auth.guard';
 import { UsuarioListComponent } from './components/usuario/usuario-list/usuario-list.component';
 import { HeaderComponent } from './components/header/header.component';
 import { UsuarioCreateComponent } from './components/usuario/usuario-create/usuario-create.component';
+import { UsuarioEditComponent } from './components/usuario/usuario-edit/usuario-edit.component';
 
 
 
@@ -19,6 +20,7 @@ const routes: Routes = [
 
       {path: 'usuarios', component: UsuarioListComponent},
       {path: 'usuarios/create', component: UsuarioCreateComponent},
+      {path: 'usuarios/edit', component: UsuarioEditComponent},
    
     ]
   },
