7f3860f4ebf91e03c05cb1b0c8e707bfc868273b
IgorDv5 <igoraluno2005@gmail.com>
2024-03-17 23:08:21 -0300

    Finalizando Componente Header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -15,7 +15,7 @@ export class HeaderComponent implements OnInit {
     private toast: ToastrService) { }
 
   ngOnInit(): void {
-    this.router.navigate(['carros'])
+    this.router.navigate(['home'])
   }
 
   logout() {
